5436574b1fd7efdf5cbd5970cfcecede70a305fb
laja99 <layla.jaber.m@gmail.com>
2026-04-14 09:42:02 +0300

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+"""
+This module provides a basic type-annotated function for mathematical operations.
+It is designed to demonstrate how to use float annotations in Python 3.
+"""
 def add(a: float, b: float) -> float:
     """Return the sum of a and b as a float."""
     return a + b
